35d8d0373b481fbdbbdff8f63b71e4ff6be21d64
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-08-24 15:55:56 +0300

    Handle user signup

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = 5555;
 
+app.use(express.json());
 app.use('/api/auth' , authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
