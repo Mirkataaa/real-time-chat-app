a0328a39834121de14b894b4d3326b1f0905f454
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-08-29 11:10:34 +0300

    Implement message rotues

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
 
 
@@ -12,6 +13,7 @@ const PORT = 5555;
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth' , authRoutes);
+app.use('/api/message' , messageRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
   connectDB();
