c4b7b337cdf985cc8ae5b94bff79193525137e0d
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-08-30 19:30:06 +0300

    Install & config cors , axios & zustand , protect routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDB } from './lib/db.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
@@ -12,8 +12,15 @@ const PORT = 5555;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth' , authRoutes);
-app.use('/api/message' , messageRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
   connectDB();
