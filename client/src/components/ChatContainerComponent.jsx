47b3f8234563698787cdc461efdfe7535e780ef2
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-09-04 16:49:53 +0300

    feat(ui,store): style chat container & chat header components , add dummy array for online users

--- client/src/components/ChatContainerComponent.jsx
+++ client/src/components/ChatContainerComponent.jsx
@@ -1,5 +1,26 @@
-export default function ChatContainerComponent () {
+import { useChatStore } from '../store/useChatStore'
+import { useEffect } from 'react';
+import ChatHeaderComponent from './ChatHeaderComponent';
+import MessageInputComponent from './MessageInputComponents';
+
+export default function ChatContainerComponent() {
+
+    const { messages, getMessages, isMesagesLoading, selectedUser } = useChatStore();
+
+    useEffect(() => {
+        getMessages(selectedUser._id)
+    }, [selectedUser._id, getMessages])
+
+    if (isMesagesLoading) return <div>Loading...</div>
+
+
     return (
-        <div>Hello from chat container component</div>
+        <div className='flex-1 flex flex-col overflow-auto'>
+            <ChatHeaderComponent />
+
+            <p>messages...</p>
+
+            <MessageInputComponent />
+        </div>
     );
 };
