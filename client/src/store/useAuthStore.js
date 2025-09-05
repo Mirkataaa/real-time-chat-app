088bbe34ae5644cc6cc19c774c6f889d43179766
Miroslav Peev <m1r0n4ouz@gmail.com>
2025-09-05 09:40:04 +0300

    feat(socket): implement basic socket.io connection and disconnection handling

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,19 +1,24 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
-export const useAuthStore = create((set) => ({
+const BASE_URL = "http://localhost:5555";
+
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
   isCheckingAuth: true,
   onlineUsers: [],
+  socket: null,
 
   checkAuth: async () => {
     try {
       const res = await axiosInstance.get("/auth/check");
       set({ authUser: res.data });
+      get().connectSocket();
     } catch (error) {
       console.log("Error in checkAuth:", error);
 
@@ -29,6 +34,7 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -42,6 +48,8 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
+
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -54,22 +62,35 @@ export const useAuthStore = create((set) => ({
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
+      get().disconnectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     }
   },
 
   updateProfile: async (data) => {
-    set({isUpdatingProfile: true});
+    set({ isUpdatingProfile: true });
     try {
-      const res = await axiosInstance.put('/auth/update-profile' , data);
-      set({authUser: res.data});
-      toast.success('Profile picture updated successfully!')
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile picture updated successfully!");
     } catch (error) {
-      console.log( 'Error in update profile' ,error);
+      console.log("Error in update profile", error);
       toast.error(error.response.data.message);
     } finally {
-      set({isUpdatingProfile: false});
+      set({ isUpdatingProfile: false });
     }
+  },
+
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL);
+    socket.connect();
+    set({socket: socket})
+  },
+  disconnectSocket: () => {
+    if(get().socket?.connected) get().socket.disconnect();
   },
 }));
--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,9 @@ import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+import {app , server} from './lib/socket.js'
 
 dotenv.config();
-const app = express();
 const PORT = 5555;
 
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
   connectDB();
 });
